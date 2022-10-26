83306557c83c81f08ae9200cd61f23c702000008
valar <68338647+76616c6172@users.noreply.github.com>
2022-10-26 16:30:37 -0700

    Wed Oct 26 04:30:37 PM PDT 2022

--- declarative-env.nix
+++ declarative-env.nix
@@ -1,8 +1,9 @@
 let nixpkgs = import <nixpkgs> { }; in
 let _pkgs = import <nixpkgs> { }; in
 rec { _paths = [
-nixpkgs.bat
              ]; 
       env = _pkgs.buildEnv {
       name = ''declarative-collection''; 
       paths = _paths;
+}; }
+# Updated successfully!
