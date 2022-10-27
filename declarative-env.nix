5e1ba09e473d499e885dffa586aaef6706048f0d
valar <68338647+76616c6172@users.noreply.github.com>
2022-10-26 21:35:16 -0700

    Wed Oct 26 09:35:16 PM PDT 2022

--- declarative-env.nix
+++ declarative-env.nix
@@ -1,6 +1,7 @@
 let nixpkgs = import <nixpkgs> { }; in
 let _pkgs = import <nixpkgs> { }; in
 rec { _paths = [
+btop
 nixpkgs.bat
 nixpkgs.git
 nixpkgs.neovim
@@ -9,4 +10,3 @@ nixpkgs.neovim
       name = ''declarative-collection''; 
       paths = _paths;
 }; }
-# Updated successfully!
